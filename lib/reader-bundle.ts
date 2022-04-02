53718478abaf20abf8ddab8e12f09dbcd31bfb39
Milo <54866561+MiloTheirself@users.noreply.github.com>
2022-04-02 21:17:15 +0200

    * debug

--- lib/reader-bundle.ts
+++ lib/reader-bundle.ts
@@ -2,15 +2,15 @@ import { default as watcher } from './module/watcher.ts';
 
 import { resolve } from 'https://deno.land/std@0.132.0/path/mod.ts';
 
-// watcher.connectedCallback({
-//   source: resolve('./'),
-//   output: resolve('./.github/reader'),
-// });
+watcher.connectedCallback({
+  source: resolve('./'),
+  output: resolve('./.github/reader'),
+});
 
-// watcher.whenConnected().then(async () => {
-//   await watcher.disconnectedCallback();
+watcher.whenConnected().then(async () => {
+  await watcher.disconnectedCallback();
 
-//   Deno.exit();
-// });
+  Deno.exit();
+});
 
 console.log('foo');
