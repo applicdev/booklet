70a7bad2fcfe5bda7ae60ee097cce86bdf4ede09
Milo <54866561+MiloTheirself@users.noreply.github.com>
2022-04-02 21:41:06 +0200

    * debug

--- lib/module/watcher.ts
+++ lib/module/watcher.ts
@@ -1,6 +1,6 @@
 // import { default as publics } from './watcher/watcher-publics.ts';
 // import { default as statics } from './watcher/watcher-statics.ts';
-// import { default as helpers } from './helpers/index.ts';
+import { default as helpers } from './helpers/index.ts';
 
 // import * as file from 'https://deno.land/std@0.132.0/fs/mod.ts';
 // import * as path from 'https://deno.land/std@0.132.0/path/mod.ts';
@@ -12,27 +12,27 @@ internal.connect = new Promise((res) => (internal.resolveConnected = res));
 fragment.whenConnected = (): Promise<void> => internal.connect;
 
 fragment.connectedCallback = async ({ source, output }: any): Promise<void> => {
-  // const option = { source, output };
-  console.log('connectedCallback');
+  const option = { source, output };
 
-  // const whenChanged = async () => {
-  //   // + create and clear output folder
-  //   // console.log(option.output);
-  //   // await file.emptyDir(path.resolve(option.output));
-  //   // // + query data
-  //   // const content = await internal.readContent({ urn: 'content' });
-  //   // const pattern = await internal.readPattern({ urn: 'pattern' });
-  //   // // + create files
-  //   // await publics.create({ option, content, pattern });
-  //   // await statics.create({ option, content, pattern });
+  const whenChanged = async () => {
+    //   // + create and clear output folder
+    //   // console.log(option.output);
+    //   // await file.emptyDir(path.resolve(option.output));
+    //   // // + query data
+    //   // const content = await internal.readContent({ urn: 'content' });
+    //   // const pattern = await internal.readPattern({ urn: 'pattern' });
+    //   // // + create files
+    //   // await publics.create({ option, content, pattern });
+    //   // await statics.create({ option, content, pattern });
 
-  // helpers.audit('Watcher', 'bundle completed');
-  // };
+    helpers.audit('Watcher', 'bundle completed');
+    console.log(option);
+  };
 
-  // await whenChanged();
+  await whenChanged();
 
-  // internal.watchDirectories({ urn: ['pattern', 'content'], whenChanged });
-  // internal.resolveConnected();
+  internal.watchDirectories({ urn: ['pattern', 'content'], whenChanged });
+  internal.resolveConnected();
 };
 
 fragment.disconnectedCallback = async () => {
