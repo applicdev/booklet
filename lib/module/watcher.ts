8995b17e655bcc3ddbc618f16a8a44a9a728e3b6
Milo <54866561+MiloTheirself@users.noreply.github.com>
2022-04-02 21:39:21 +0200

    * debug

--- lib/module/watcher.ts
+++ lib/module/watcher.ts
@@ -1,9 +1,9 @@
-import { default as publics } from './watcher/watcher-publics.ts';
-import { default as statics } from './watcher/watcher-statics.ts';
-import { default as helpers } from './helpers/index.ts';
+// import { default as publics } from './watcher/watcher-publics.ts';
+// import { default as statics } from './watcher/watcher-statics.ts';
+// import { default as helpers } from './helpers/index.ts';
 
-import * as file from 'https://deno.land/std@0.132.0/fs/mod.ts';
-import * as path from 'https://deno.land/std@0.132.0/path/mod.ts';
+// import * as file from 'https://deno.land/std@0.132.0/fs/mod.ts';
+// import * as path from 'https://deno.land/std@0.132.0/path/mod.ts';
 
 const fragment: { [prop: string]: any } = {};
 const internal: { [prop: string]: any } = {};
