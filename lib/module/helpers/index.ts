7614d6b6640a35a49f45d898a40bbc1cbddfd52c
Milo <54866561+MiloTheirself@users.noreply.github.com>
2022-04-02 22:52:06 +0200

    * clean-up

--- lib/module/helpers/index.ts
+++ lib/module/helpers/index.ts
@@ -1,25 +1,10 @@
 import * as Colors from 'https://deno.land/std@0.132.0/fmt/colors.ts';
-// import Ask from 'https://deno.land/x/ask@1.0.6/mod.ts';
 
 const fragment: { [prop: string]: any } = {};
 const internal: { [prop: string]: any } = {};
 
-// fragment.confirm = async (plain: string): Promise<boolean> => {
-//   const result = await new Ask().prompt([
-//     {
-//       name: 'value',
-//       message: plain,
-//       type: 'confirm',
-//     },
-//   ]);
-
-//   console.log(result);
-
-//   return !!result.value;
-// };
-
 fragment.audit = async (plain: string, start?: boolean): Promise<void> => {
-  console.log(start ? Colors.green('\ne ') : '+ ', plain);
+  console.log(start ? Colors.green('\ne ') : Colors.gray('+ '), plain);
 };
 
 export default { ...fragment };
