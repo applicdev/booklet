cfd11af829f4599894d66ddba357f80e4618a6a2
Milo <54866561+MiloTheirself@users.noreply.github.com>
2022-04-27 09:22:02 +0200

    * clean-up

--- lib/module/snippet/local/local-date.ts
+++ lib/module/snippet/local/local-date.ts
@@ -1,7 +1,7 @@
 const fragment: { [prop: string]: any } = {};
 const internal: { [prop: string]: any } = {};
 
-fragment.create = ({ time }: { time: number }) => {
+fragment.create = ({ val }: { val: number | Date }) => {
   return `00/00/00`;
 };
 
