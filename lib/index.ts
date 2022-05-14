5b131506bce61789fd3c9ee36bef21b3e5d5aa9b
Milo <54866561+MiloTheirself@users.noreply.github.com>
2022-05-14 14:34:16 +0200

    * clean-up

--- lib/index.ts
+++ lib/index.ts
@@ -1,59 +1,73 @@
 import './typeset/typeset-interface.ts';
 import './typeset/typeset-workflows.ts';
 
-import { bundle, stream } from './mod.ts';
-
+import { default as workers } from './mod.ts';
 import { default as snippet } from './snippet/index.ts';
-// import { default as watcher } from './module/interface/interface-watcher.ts';
-// import { default as streams } from './module/interface/interface-streams.ts';
 
 const flag = await snippet.flag.resolve(Deno.args);
-const option: InterfaceOption = {
+
+// ? confirm working directory
+if ('v' in flag || 'version' in flag) {
+  const plain = `v0.1.2-experimental`;
+
+  snippet.print.info(plain);
+  Deno.exit();
+}
+
+// ? confirm working directory
+if ('h' in flag || 'help' in flag) {
+  const plain = `Usage: reader [options]
+
+Options:
+  -v, --version       output the version number
+  -h, --help          output usage information
+
+  -s, --stream        start a local server for bundled assets
+  -p                  use repository name as path namespace
+  -f                  force file writes
+`;
+
+  snippet.print.info(plain);
+  Deno.exit();
+}
+
+// ? confirm working directory
+if (!('f' in flag || 'force' in flag)) {
+  const paths = snippet.print.bold(snippet.path.resolve());
+  const plain = `You're about to initialize in this directory:\n\n  ${paths}\n`;
+  snippet.print.info(plain);
+
+  // ? abort; in case of wrong working directory
+  if (!(await snippet.input.confirm(`Are you ready to proceed?`))) {
+    snippet.print.fail('Aborted by user.');
+    Deno.exit();
+  }
+}
+
+// ? initialize bundle or bundle and stream
+const { worker } = { worker: 's' in flag || 'stream' in flag ? workers.stream : workers.bundle };
+const { source, output, hosted } = {
   source: { urn: snippet.path.resolve('./') },
   output: { urn: snippet.path.resolve('./.github/workflows-output') },
   hosted: {
     urn: snippet.path.resolve('./.github/workflows-hosted'),
-
-    // ? public path
     path: await (async () => {
       let path = 'p' in flag ? (flag['p'] as any) : (flag['public-path'] as any);
 
       // ? try to use the repo name; when not path defind
-      if (!(path instanceof String)) {
+      if (typeof path != 'string' && path) {
         const repo = await snippet.repo.requestConfig();
-
-        if (repo && !repo.name.endsWith('github.io.git')) {
-          path = repo.name;
-        }
+        if (repo && !repo.name.endsWith('github.io.git')) path = repo.name;
       }
 
       // ---
-      // FIXME: better validate the path
+      // FIXME: better validate the hosted path
       if (typeof path != 'string') path = '';
       return `/${path.replace(/[^a-zA-Z0-9-_]/g, '')}/`.replace(/\/\//g, '/');
       // ---
     })(),
   },
 };
-
-// ? confirm working directory
-{
-  if (!('f' in flag || 'force' in flag)) {
-    const paths = snippet.print.bold(snippet.path.resolve());
-    const plain = `You're about to initialize in this directory:\n\n  ${paths}\n`;
-    snippet.print.info(plain);
-
-    // ? abort; when wrong working directory path
-    if (!(await snippet.input.confirm(`Are you ready to proceed?`))) {
-      snippet.print.fail('Aborted by user.');
-      Deno.exit();
-    }
-  }
-}
-
-// ? initialize bundle or bundle and stream
-const { worker } = { worker: !('stream' in flag) ? bundle : stream };
-const { source, output, hosted } = option;
 
 for await (const res of worker({ source, output, hosted })) {
   // ?
--- lib/mod.ts
+++ lib/mod.ts
@@ -1,40 +1,35 @@
 import './typeset/typeset-interface.ts';
 import './typeset/typeset-workflows.ts';
 
-import { default as snippet } from './snippet/index.ts';
+// import { default as snippet } from './snippet/index.ts';
 import { default as watcher } from './module/interface/interface-watcher.ts';
 import { default as streams } from './module/interface/interface-streams.ts';
 
 export async function* bundle({ source, output, hosted }: InterfaceOption): InterfaceGenerator {
-  console.log({ source, output, hosted });
+  watcher.connectedCallback({ source, output, hosted });
+  watcher.whenConnected().then(async () => {
+    await watcher.disconnectedCallback();
+    Deno.exit();
+  });
 
   yield {};
-
-  return;
 }
 
 export async function* stream({ source, output, hosted }: InterfaceOption): InterfaceGenerator {
-  console.log({ source, output, hosted });
+  watcher.connectedCallback({ source, output, hosted });
+  watcher.whenConnected().then(async () => {
+    streams.connectedCallback({ source, output, hosted });
+
+    // ---
+    // FIXME: find a better implementation; disconnected has to run before deno close
+    globalThis.addEventListener('unload', async () => {
+      await streams.disconnectedCallback();
+      await watcher.disconnectedCallback();
+    });
+    // ---
+  });
 
   yield {};
-
-  return;
 }
-
-// // ? initialize bundle or bundle and stream
-// watcher.connectedCallback({ ...option });
-// watcher.whenConnected().then(async () => {
-//   if (!('stream' in optionInterface)) {
-//     await watcher.disconnectedCallback();
-//     Deno.exit();
-//   }
-
-//   // ---
-//   streams.connectedCallback({ ...option });
-//   // FIXME: find a better implementationl; disconnected has to run on deno close
-//   // streams.disconnectedCallback();
-//   // watcher.disconnectedCallback();
-//   // ---
-// });
 
 export default { bundle, stream };
