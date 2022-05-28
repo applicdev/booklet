9cbfd9a40773031627560d83828665540d64318a
Milo <54866561+MiloTheirself@users.noreply.github.com>
2022-05-28 22:36:11 +0200

    * clean-up

--- lib/worker/workers/final/final-preview.ts
+++ lib/worker/workers/final/final-preview.ts
@@ -1,5 +1,7 @@
 import { default as snippet } from '../../snippet/index.ts';
+
 import puppeteer from 'https://deno.land/x/puppeteer@14.1.1/mod.ts';
+import { encodeUrl } from 'https://deno.land/x/encodeurl/mod.ts';
 
 const fragment: { [prop: string]: any } = {};
 const internal: { [prop: string]: any } = {};
@@ -10,33 +12,63 @@ fragment.request = async ({ bundle, option }: any): Promise<void> => {
 
   // ?
   const browser = await puppeteer.launch({
-    product: 'firefox',
+    // product: 'firefox',
     headless: true,
-    defaultViewport: { width: 1200, height: 628 },
+    defaultViewport: { width: 1538, height: 923, deviceScaleFactor: 2 },
   });
   const page = await browser.newPage();
 
-  // const pag = pages[0];
-
-  // // for (const pag of pages) {
-  // //   try {
   const key = '/reader/manual/getting-started'.replace(/^\/|\/$/g, '').replace(/\//g, '-');
-  // // const url = 'http://localhost:8080' + pag.urn;
-  const url = 'https://example.com/';
+  const url = 'data:text/html;charset=UTF-8,';
   const out = snippet.path.resolve(option.hosted.urn, `./~/preview/${key}.png`);
 
-  // console.log({ key, url, out });
-
-  console.log(out);
-
   await page.goto(url);
-  await page.screenshot({ path: out });
-  // // } catch (err) {
-  // //   console.log(err);
-  // // }
-  // // }
+  await page.setContent(await internal.render());
+  await page.screenshot({
+    path: out,
+    clip: {
+      x: 169,
+      y: 0,
+      width: 1200,
+      height: 628,
+    },
+  });
 
   await browser.close();
+};
+
+internal.render = async (): Promise<string> => {
+  return `
+    <style>
+      html {
+        font-size: 16px;
+      }
+     
+      body {
+        display: grid;
+        justify-content: center;
+        align-content: center;
+
+        grid-auto-flow: row;
+        gap: 1.25rem;
+        padding: 1.25rem;
+        
+        background: #f2f2f2;
+        margin: 0rem 0rem;
+      }
+
+      section {
+        background: #fcfcfc;
+        border-radius: 2px;
+        outline: 1px solid #e9e9e9;
+
+        width: 794px;
+        height: 1123px;
+      }
+    </style>
+
+    <section></section>
+  `;
 };
 
 export default { ...fragment };
