b24052bd8cae3792c9d1b742a4904db86c086517
Milo <54866561+MiloTheirself@users.noreply.github.com>
2022-05-30 20:11:24 +0200

    * clean-up

--- lib/worker/workers/final/final-printed.ts
+++ lib/worker/workers/final/final-printed.ts
@@ -4,18 +4,14 @@ const fragment: { [prop: string]: any } = {};
 const internal: { [prop: string]: any } = {};
 
 fragment.request = async ({ page, urn }: any): Promise<void> => {
-  const env = Deno.env.toObject();
-
-  if (!('OS' in env) || env.OS != 'Windows_NT') {
-    await page.emulateMediaType('print');
-    await page.pdf({
-      path: urn,
-      format: 'A4',
-      printBackground: true,
-      displayHeaderFooter: false,
-      timeout: 10000,
-    });
-  }
+  // await page.emulateMediaType('print');
+  // await page.pdf({
+  //   path: urn,
+  //   format: 'A4',
+  //   printBackground: true,
+  //   displayHeaderFooter: false,
+  //   timeout: 10000,
+  // });
 };
 
 export default { ...fragment };
