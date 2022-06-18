f36d27961524cb8b6deedff69c189a85f5fbba06
milotheirself <milotheirself@users.noreply.github.com>
2022-06-18 06:04:18 +0000

    deploy: 2786d55752118a8e1b24d66d9172df8d56934138

--- assets/modules/booklet-inline.js
+++ assets/modules/booklet-inline.js
@@ -1,14 +1,23 @@
+import { html, svg, render } from 'https://unpkg.com/lit-html?module';
+
 const fragment = {};
 const internal = {};
 
 /* */
 
+fragment.snapToBanner = () => {
+  let hei = document.querySelector('.node.banner').offsetHeight;
+  document.querySelector('.node.bounds > .bounds-inner').scroll(0, hei);
+};
+
+/* */
+
 internal.requestScrollUpdate = () => {
   // ? banner visibility
-  let hei = document.querySelector('.node.banner > .banner-content').offsetHeight / 3;
+  let hei = document.querySelector('.node.banner > .banner-content').offsetHeight;
   let pos = document.querySelector('.node.bounds > .bounds-inner').scrollTop;
 
-  let vis = Math.max(hei - pos + 50, 0) / hei;
+  let vis = Math.max(80 - pos + hei / 2, 0) / 80;
   document.querySelector('.node.banner').setAttribute('style', `--banner-vis: ${vis};`);
 };
 
@@ -16,18 +25,16 @@ globalThis.addEventListener('scroll', internal.requestScrollUpdate, true);
 
 /* */
 
-internal.debugRender = ({ reader, option }) => {
-  const { hosted, bundle } = globalThis.booklet;
-
-  return `
+internal.template = ({ reader, banner }) => {
+  return html`
     <div class="node bounds">
       <div class="bounds-inner">
         <header class="node banner">
           <div class="banner-content">
             <!---->
             <div class="type-block">
-              <span class="type banner">Booklet</span>
-              <span class="type banner-caption">Debugging-Document</span>
+              <span class="type banner">${banner.title}</span>
+              <span class="type banner-caption">${banner.caption}</span>
             </div>
             <!---->
           </div>
@@ -35,16 +42,20 @@ internal.debugRender = ({ reader, option }) => {
             <!---->
             <div>
               <!---->
-              <a class="action-icon" href="${booklet.hosted.path}">
-                <span class="type icon">${internal.debugRenderIcon({ name: 'paper:previous' })}</span>
-              </a>
+              ${banner.action[0].map(
+                (act) => html`
+                  <a class="action-icon" href="${banner.path}">
+                    <span class="type icon">${internal.templateIcon({ name: act.icon })}</span>
+                  </a>
+                `
+              )}
               <!---->
             </div>
             <div>
               <!---->
               <div class="type-block-sm">
-                <span class="type action">Booklet</span>
-                <span class="type action-sm">Debugging-Document</span>
+                <span class="type action">${banner.title}</span>
+                <span class="type action-sm">${banner.caption}</span>
               </div>
               <!---->
             </div>
@@ -56,94 +67,64 @@ internal.debugRender = ({ reader, option }) => {
           </div>
         </header>
 
-        ${
-          !reader
-            ? `
-              <main>
-
-              </main>
-              `
-            : `
-              <main class="node reader">
-                <!---->
-                ${[{}, {}, {}]
-                  .map(
-                    (sec) => `
-                    <section class="reader-section"></section>
-                  `
-                  )
-                  .join('')}
-                <!---->
-              </main>
-              `
-        }
+        <main class="node reader">
+          ${!reader
+            ? `test`
+            : [{}, {}, {}].map(
+                (sec) => html`
+                  <!---->
+                  <section class="reader-section"></section>
+                  <!---->
+                `
+              )}
+        </main>
       </div>
     </div>
   `;
 };
 
-internal.debugRenderIcon = ({ name }) => {
-  return `
-    <svg 
-      xmlns="http://www.w3.org/2000/svg" 
-      width="24" 
-      height="24" 
-      viewBox="0 0 24 24" 
-      stroke-width="1.6" 
-      stroke="currentColor" 
-      fill="none" 
-      stroke-linecap="round" 
-      stroke-linejoin="round"
-    >
-      ${
-        {
-          'paper:previous': `
+internal.templateIcon = ({ name }) => {
+  return html`
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.6" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+      ${{
+        'paper:previous': svg`
             <polyline class="st1" points="15.7,19.3 8.2,11.9 15.7,4.4 "/>
           `,
-          'paper:navigate': `
+        'paper:navigate': svg`
             <line class="st0" x1="5.4" y1="12" x2="18.4" y2="12"/>
             <line class="st0" x1="5.4" y1="5.5" x2="18.4" y2="5.5"/>
             <line class="st0" x1="5.4" y1="18.5" x2="18.4" y2="18.5"/>
           `,
-          'paper:upcoming': `
-            <path d="M20 12v-6a2 2 0 0 0 -2 -2h-12a2 2 0 0 0 -2 2v8" />
-            <path d="M4 18h17" />
-            <path d="M18 15l3 3l-3 3" />
-          `,
-          'paper:anker': `
+        'paper:anker': svg`
             <circle cx="6" cy="19" r="2"></circle>
             <circle cx="18" cy="5" r="2"></circle>
             <path d="M12 19h4.5a3.5 3.5 0 0 0 0 -7h-8a3.5 3.5 0 0 1 0 -7h3.5"></path>
           `,
-          'paper:close': `
-            <path d="M21 12v3a1 1 0 0 1 -1 1h-16a1 1 0 0 1 -1 -1v-10a1 1 0 0 1 1 -1h9" />
-            <line x1="7" y1="20" x2="17" y2="20" />
-            <line x1="9" y1="16" x2="9" y2="20" />
-            <line x1="15" y1="16" x2="15" y2="20" />
-            <path d="M17 8l4 -4m-4 0l4 4" />
-          `,
-        }[name]
-      }
+      }[name]}
     </svg>
   `;
 };
-// ---
 
-globalThis.addEventListener('DOMContentLoaded', async () => {
-  document.body.innerHTML = internal.debugRender({
-    reader: {}, //false,
-    option: {},
-  });
+/* */
 
-  // ? snap to header
-  if (location.hash == '') {
-    if ('scrollRestoration' in history) history.scrollRestoration = 'manual';
+render(
+  internal.template({
+    banner: {
+      title: 'Booklet',
+      caption: 'Debugging-Document',
+      action: [
+        [{ icon: 'paper:previous', type: 'show:previous' }], //
+        [{ icon: 'paper:previous', type: 'show:previous' }],
+      ],
+    },
+    reader: {},
+  }),
+  document.body
+);
 
-    let hei = document.querySelector('.node.banner').offsetHeight;
-    document.querySelector('.node.bounds > .bounds-inner').scroll(0, hei);
+requestAnimationFrame(() => {
+  if (location.hash != '') return;
+  if ('scrollRestoration' in history) history.scrollRestoration = 'manual';
 
-    requestAnimationFrame(() => {
-      document.body.removeAttribute('active-scroll');
-    });
-  }
+  fragment.snapToBanner();
 });
